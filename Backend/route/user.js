d8c851da61f92b1f08775f3176d1a977ca1f5c0e
ethio-man <natymiskir@gmail.com>
2026-01-21 15:32:08 -0300

    fetching single user

--- Backend/route/user.js
+++ Backend/route/user.js
@@ -2,6 +2,20 @@ import express from "express";
 
 const route = express.Router();
 
-route.get("/", async () => {});
+route.get("/", async (req, res) => {
+  try {
+    //fetch logic will be here
+  } catch (err) {
+    //server error will be handled here
+  }
+});
+
+route.get("/:id", async (req, res) => {
+  try {
+    //getting by id logic
+  } catch (err) {
+    //server error will be handled here
+  }
+});
 
 export default route;
