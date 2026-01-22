531397e9e8a2a267487647d3fd30313c60c4a4ac
ethio-man <natymiskir@gmail.com>
2026-01-22 11:24:28 -0300

    setup to read products

---
+++ Backend/route/product.js
@@ -0,0 +1,10 @@
+import express from "express";
+const route = express.Router();
+
+route.get("/", async (req, res) => {
+  try {
+    //find products logic
+  } catch (err) {
+    //server error catch here
+  }
+});
