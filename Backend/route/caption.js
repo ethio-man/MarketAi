50f5cea9e7851d3d66e93489c41ef72c2e2d92ff
ethio-man <natymiskir@gmail.com>
2026-01-23 06:15:41 -0300

    setup to find all captions

---
+++ Backend/route/caption.js
@@ -0,0 +1,12 @@
+import express from "express";
+const route = express.Router();
+
+route.get("/", async (req, res) => {
+  try {
+    //get caption logic here
+  } catch (err) {
+    //catch server error here
+  }
+});
+
+export default route;
