f318959c33357c6bc1b6a8fc6263f784432ffdd1
ethio-man <natymiskir@gmail.com>
2026-01-24 15:44:43 -0300

    server listening the port

--- Backend/index.js
+++ Backend/index.js
@@ -1,2 +1,8 @@
 import express from "express";
 const app = express();
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+  console.log(`Server is listening on the port ${port} ...`);
+});
+export default server;
