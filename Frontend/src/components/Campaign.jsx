8bc9e0a05f13a20753e331b2a51cdf5489534bc7
ethio-man <natymiskir@gmail.com>
2026-03-13 16:39:54 -0300

    product list changed to input type

--- Frontend/src/components/Campaign.jsx
+++ Frontend/src/components/Campaign.jsx
@@ -67,20 +67,8 @@ const CampaignBuilder = () => {
                 Choose a product to promote
               </label>
               <div className="relative">
-                <select className="w-full p-3 border border-gray-300 rounded-lg appearance-none bg-white focus:outline-none focus:ring-2 focus:ring-blue-500">
-                  <option>Select a product</option>
-                  <option>Sidamo Coffee</option>
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  {/* Down Arrow Icon */}
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </div>
+                <input className="w-full p-3 border border-gray-300 rounded-lg appearance-none bg-white focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"></div>
               </div>
             </div>
 
